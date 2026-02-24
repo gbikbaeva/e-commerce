661eb94b812f92a1d52f85bd2ef18c523fce19ab
ru128gulbikb <g.bikbaeva@crif.com>
2026-02-24 21:55:46 +0100

    feat: add collections section

---
+++ src/components/Collections/Collections.jsx
@@ -0,0 +1,61 @@
+import { useCallback, useEffect, useState } from "react";
+import clsx from "clsx";
+
+import CollectionCard from "./CollectionCard";
+
+const Collections = () => {
+  const [collections, setCollections] = useState([]);
+  const [isLoadingCollections, setIsLoadingCollections] = useState(false);
+
+  const getCollections = useCallback(async () => {
+    setIsLoadingCollections(true);
+    const response = await fetch("/api/collections");
+    const result = await response.json();
+    setCollections(result.data);
+    setIsLoadingCollections(false);
+  }, []);
+
+  useEffect(() => {
+    getCollections();
+  }, [getCollections]);
+
+  return (
+    <div
+      className={clsx(
+        "w-full px-3 py-12 md:px-4 md:py-16 lg:px-24 lg:py-24",
+        "flex flex-col gap-8",
+      )}
+    >
+      <p className="text-3xl font-semibold">Our Collections</p>
+
+      {isLoadingCollections ? (
+        <div>Loading...</div>
+      ) : (
+        collections.length > 2 && (
+          <div className="flex gap-7 flex-col md:flex-row">
+            <div className="flex-1">
+              <CollectionCard collection={collections[0]}></CollectionCard>
+            </div>
+
+            <div className="flex flex-col gap-7 flex-1">
+              <div className="flex-1">
+                <CollectionCard
+                  collection={collections[1]}
+                  variant="secondary"
+                ></CollectionCard>
+              </div>
+              <div className="flex-1">
+                <CollectionCard
+                  collection={collections[2]}
+                  variant="secondary"
+                ></CollectionCard>
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default Collections;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Collections from "../components/Collections";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <></>;
+  return <Collections />;
 }
